d1e4ac758c64d66882d500124ff38b49df45b79c
Keshav <vkeshav300@gmail.com>
2022-11-20 18:09:52 -0800

    Added a SoundManager class to assets.h and assets.cpp

--- src/assets.cpp
+++ src/assets.cpp
@@ -3,7 +3,7 @@
 #include "assets.h"
 
 // Loading a texture
-SDL_Texture* TextureManager::LoadTexture(const char* filename) {
+SDL_Texture* TextureManager::LoadTexture(const char *filename) {
     SDL_Surface *tempSurface = IMG_Load(filename);
     SDL_Texture *tex = SDL_CreateTextureFromSurface(Game::renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
@@ -15,3 +15,7 @@ SDL_Texture* TextureManager::LoadTexture(const char* filename) {
 void TextureManager::Draw(SDL_Texture *tex, SDL_Rect src, SDL_Rect dest) {
     SDL_RenderCopy(Game::renderer, tex, &src, &dest);
 }
+
+void SoundManager::LoadSound(const char *filename) {}
+
+void SoundManager::Play() {}
--- src/assets.h
+++ src/assets.h
@@ -3,6 +3,12 @@
 
 class TextureManager {
     public:
-        static SDL_Texture* LoadTexture(const char* filename);
+        static SDL_Texture* LoadTexture(const char *filename);
         static void Draw(SDL_Texture *tex, SDL_Rect src, SDL_Rect dest);
+};
+
+class SoundManager {
+    public:
+        static void LoadSound(const char *filename);
+        static void Play();
 };
